31c0f769b864b61dc26bfebb8b9cf374d9e6a656
FX-Wood <fxwood2@gmail.com>
2019-02-10 10:48:54 -0800

    adding second test

--- test/test.js
+++ test/test.js
@@ -12,3 +12,18 @@ describe('makeSupply()', function() {
         assert.hasAllKeys(supply, ["basic", "kingdom", "trash"], "does not have three keys")
     })
 })
+
+var CARDS = require('../js/resources.js').CARDS
+describe("CARDS", function() {
+    it('should be an object', function() {
+        assert.typeOf(CARDS, "object", "not an object")
+    })
+    for (let card in CARDS) {
+        it(`${card} should be an array with length 8`, function() {
+            assert.typeOf(CARDS[card], "array", `${card} not an array`)
+            assert.lengthOf(CARDS[card], 8, `${card} has only ${CARDS[card].length} elements`)
+            assert.typeOf(CARDS[card][0], 'string', `${card} name field is not a string` )
+        })
+    }
+
+})
